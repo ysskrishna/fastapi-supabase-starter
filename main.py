c4376a9f58efb6a382337e6d92df768201c0f641
Siva Sai <sivasaikrishnassk@gmail.com>
2025-06-07 10:31:19 +0530

    Siva | migrate existing project to use uv package manager

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def read_root():
 app.include_router(user.router, prefix="/user", tags=["user"])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=8000)
